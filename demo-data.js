74d17117433fef751a1b781ef951cb4ba3ac71d0
mathuramg <mmg9291@gmail.com>
2016-04-23 15:51:05 -0400

    initial commit

---
+++ demo-data.js
@@ -0,0 +1,376 @@
+var xOffset = 17;
+var yOffset = 18;
+var roomData = [
+  {name:"all1", // 64 82
+  id: -1,
+  points: [{ "x": 0 + xOffset,   "y": 0 + yOffset},
+      { "x": 741 + xOffset,   "y": 0 + yOffset},
+      { "x": 741 + xOffset,  "y": 173 + yOffset},
+      { "x": 0 + xOffset,  "y": 173 + yOffset},
+      { "x": 0 + xOffset,   "y": 0 + yOffset}]
+  },
+  {
+  name:"455",
+  id: 0,
+  points: [{ "x": 0 + xOffset,   "y": 0 + yOffset},
+            { "x": 58 + xOffset,   "y": 0 + yOffset},
+            { "x": 58 + xOffset,  "y": 66 + yOffset},
+            { "x": 0 + xOffset,  "y": 66 + yOffset},
+            { "x": 0 + xOffset,   "y": 0 + yOffset}]
+          },
+  {name:"454", // 58  47
+  id: 1,
+  points: [{ "x": 0 + xOffset,   "y": 0 + yOffset},
+            { "x": 58 +58 + xOffset,   "y": 0 + yOffset},
+            { "x": 58 +58 + xOffset,  "y": 47 + yOffset},
+            { "x": 58 + xOffset,  "y": 47+ yOffset},
+            { "x": 58 + xOffset,   "y": yOffset}]
+          },
+  {name:"453", // 58  47
+  id: 2,
+  points: [{ "x": 116 + xOffset,   "y": 0 + yOffset},
+            { "x": 151 + xOffset,   "y": 0 + yOffset},
+            { "x": 151 + xOffset,  "y": 47 + yOffset},
+            { "x": 116 + xOffset,  "y": 47+ yOffset},
+            { "x": 116 + xOffset,   "y": yOffset}]
+          },
+  {name:"452", // 58  47
+  id: 3,
+  points: [{ "x": 151 + xOffset,   "y": 0 + yOffset},
+            { "x": 181 + xOffset,   "y": 0 + yOffset},
+            { "x": 181 + xOffset,  "y": 47 + yOffset},
+            { "x": 151 + xOffset,  "y": 47+ yOffset},
+            { "x": 151 + xOffset,   "y": yOffset}]
+          },
+  {name:"451b", // 58  47
+  id: 4,
+  points: [{ "x": 181 + xOffset,   "y": 0 + yOffset},
+            { "x": 214 + xOffset,   "y": 0 + yOffset},
+            { "x": 214 + xOffset,  "y": 47 + yOffset},
+            { "x": 181 + xOffset,  "y": 47+ yOffset},
+            { "x": 181 + xOffset,   "y": yOffset}]
+          },
+  {name:"451a", // 58  47
+  id: 5,
+  points: [{ "x": 214 + xOffset,   "y": 0 + yOffset},
+            { "x": 247 + xOffset,   "y": 0 + yOffset},
+            { "x": 247 + xOffset,  "y": 47 + yOffset},
+            { "x": 214 + xOffset,  "y": 47+ yOffset},
+            { "x": 214 + xOffset,   "y": yOffset}]
+          },
+  {name:"449", // 58  47
+  id: 6,
+  points: [{ "x": 247 + xOffset,   "y": 0 + yOffset},
+            { "x": 278 + xOffset,   "y": 0 + yOffset},
+            { "x": 278 + xOffset,  "y": 47 + yOffset},
+            { "x": 247 + xOffset,  "y": 47+ yOffset},
+            { "x": 247 + xOffset,   "y": yOffset}]
+          },
+  {name:"448", // 58  47
+  id: 7,
+  points: [{ "x": 278 + xOffset,   "y": 0 + yOffset},
+            { "x": 316 + xOffset,   "y": 0 + yOffset},
+            { "x": 316 + xOffset,  "y": 47 + yOffset},
+            { "x": 278 + xOffset,  "y": 47+ yOffset},
+            { "x": 278 + xOffset,   "y": yOffset}]
+          },
+  {name:"446", // 58  47
+  id: 8,
+  points: [{ "x": 316 + xOffset,   "y": 0 + yOffset},
+            { "x": 370 + xOffset,   "y": 0 + yOffset},
+            { "x": 370 + xOffset,  "y": 47 + yOffset},
+            { "x": 316 + xOffset,  "y": 47+ yOffset},
+            { "x": 316 + xOffset,   "y": yOffset}]
+          },
+  {name:"lounge", // 58  47
+  id: 9,
+  points: [{ "x": 370 + xOffset,   "y": 0 + yOffset},
+            { "x": 505 + xOffset,   "y": 0 + yOffset},
+            { "x": 505 + xOffset,  "y": 83 + yOffset},
+            { "x": 370 + xOffset,  "y": 83+ yOffset},
+            { "x": 370 + xOffset,   "y": yOffset}]
+          },
+  {name:"444", // 58  47
+  id: 10,
+  points: [{ "x": 505 + xOffset,   "y": 0 + yOffset},
+            { "x": 546 + xOffset,   "y": 0 + yOffset},
+            { "x": 546 + xOffset,  "y": 59 + yOffset},
+            { "x": 505 + xOffset,  "y": 59+ yOffset},
+            { "x": 505 + xOffset,   "y": yOffset}]
+          },
+  {name:"442", // 58  47
+  id: 11,
+  points: [{ "x": 546 + xOffset,   "y": 0 + yOffset},
+            { "x": 651 + xOffset,   "y": 0 + yOffset},
+            { "x": 651 + xOffset,  "y": 59 + yOffset},
+            { "x": 546 + xOffset,  "y": 59+ yOffset},
+            { "x": 546 + xOffset,   "y": yOffset}]
+          }
+          ,
+  {name:"441", // 58  47
+  id: 12,
+  points: [{ "x": 651 + xOffset,   "y": 0 + yOffset},
+            { "x": 741 + xOffset,   "y": 0 + yOffset},
+            { "x": 741 + xOffset,  "y": 87 + yOffset},
+            { "x": 651 + xOffset,  "y": 87+ yOffset},
+            { "x": 651 + xOffset,   "y": yOffset}]
+          },
+  {name:"456", // 64 82
+  id: 13,
+  points: [{ "x": 0 + xOffset,   "y": 66 + yOffset},
+            { "x": 58 + xOffset,   "y": 66 + yOffset},
+            { "x": 58 + xOffset,  "y": 146 + yOffset},
+            { "x": 0 + xOffset,  "y": 146 + yOffset},
+            { "x": 0 + xOffset,   "y": 66 + yOffset}]
+          },
+  {name:"455a", // 64 82
+  id: 14,
+  points: [{ "x": 58 + xOffset,   "y": 47 + yOffset},
+            { "x": 116 + xOffset,   "y": 47 + yOffset},
+            { "x": 116 + xOffset,  "y": 129 + yOffset},
+            { "x": 58 + xOffset,  "y": 129 + yOffset},
+            { "x": 58 + xOffset,   "y": 47 + yOffset}]
+          },
+  {name:"440", // 64 82
+  id: 15,
+  points: [{ "x": 706 + xOffset,   "y": 87 + yOffset},
+            { "x": 741 + xOffset,   "y": 87 + yOffset},
+            { "x": 741 + xOffset,  "y": 173 + yOffset},
+            { "x": 706 + xOffset,  "y": 173 + yOffset},
+            { "x": 706 + xOffset,   "y": 87 + yOffset}]
+          },
+  {name:"behind456", // 64 82
+  id: 16,
+  points: [{ "x": 0 + xOffset,   "y": 146 + yOffset},
+            { "x": 58 + xOffset,   "y": 146 + yOffset},
+            { "x": 58 + xOffset,  "y": 173 + yOffset},
+            { "x": 0 + xOffset,  "y": 173 + yOffset},
+            { "x": 0 + xOffset,   "y": 146 + yOffset}]
+          },
+  {name:"stairs1", // 64 82
+  id: 17,
+  points: [{ "x": 58 + xOffset,   "y": 129 + yOffset},
+            { "x": 116 + xOffset,   "y": 129 + yOffset},
+            { "x": 116 + xOffset,  "y": 173 + yOffset},
+            { "x": 58 + xOffset,  "y": 173 + yOffset},
+            { "x": 58 + xOffset,   "y": 129 + yOffset}]
+          },
+  {name:"behind bathroom", // 64 82
+  id: 18,
+  points: [{ "x": 116 + xOffset,   "y": 157 + yOffset},
+            { "x": 178 + xOffset,   "y": 157 + yOffset},
+            { "x": 178 + xOffset,  "y": 173 + yOffset},
+            { "x": 116 + xOffset,  "y": 173 + yOffset},
+            { "x": 116 + xOffset,   "y": 157 + yOffset}]
+          }
+          ,
+  {name:"Mbathroom", // 64 82
+  id: 19,
+  points: [{ "x": 116 + xOffset,   "y": 81 + yOffset},
+            { "x": 146 + xOffset,   "y": 81 + yOffset},
+            { "x": 146 + xOffset,  "y": 157 + yOffset},
+            { "x": 116 + xOffset,  "y": 157 + yOffset},
+            { "x": 116 + xOffset,   "y": 81 + yOffset}]
+          },
+  {name:"Wbathroom", // 64 82
+  id: 20,
+  points: [{ "x": 146 + xOffset,   "y": 81 + yOffset},
+            { "x": 178 + xOffset,   "y": 81 + yOffset},
+            { "x": 178 + xOffset,  "y": 157 + yOffset},
+            { "x": 146 + xOffset,  "y": 157 + yOffset},
+            { "x": 146 + xOffset,   "y": 81 + yOffset}]
+          },
+  {name:"marlon", // 64 82
+  id: 21,
+  points: [{ "x": 178 + xOffset,   "y": 86 + yOffset},
+            { "x": 213 + xOffset,   "y": 86 + yOffset},
+            { "x": 213 + xOffset,  "y": 115 + yOffset},
+            { "x": 178 + xOffset,  "y": 115 + yOffset},
+            { "x": 178 + xOffset,   "y": 86 + yOffset}]
+          },
+  {name:"mechanical", // 64 82
+  id: 22,
+  points: [{ "x": 178 + xOffset,   "y": 115 + yOffset},
+            { "x": 236 + xOffset,   "y": 115 + yOffset},
+            { "x": 236 + xOffset,  "y": 173 + yOffset},
+            { "x": 178 + xOffset,  "y": 173 + yOffset},
+            { "x": 178 + xOffset,   "y": 115 + yOffset}]
+          },
+  {name:"ER", // 64 82
+  id: 23,
+  points: [{ "x": 236 + xOffset,   "y": 93 + yOffset},
+            { "x": 273 + xOffset,   "y": 93 + yOffset},
+            { "x": 273 + xOffset,  "y": 173 + yOffset},
+            { "x": 236 + xOffset,  "y": 173 + yOffset},
+            { "x": 236 + xOffset,   "y": 93 + yOffset}]
+          },
+  {name:"447", // 64 82
+  id: 24,
+  points: [{ "x": 273 + xOffset,   "y": 93 + yOffset},
+            { "x": 395 + xOffset,   "y": 93 + yOffset},
+            { "x": 395 + xOffset,  "y": 173 + yOffset},
+            { "x": 273 + xOffset,  "y": 173 + yOffset},
+            { "x": 273 + xOffset,   "y": 93 + yOffset}]
+          },
+  {name:"445", // 64 82
+  id: 25,
+  points: [{ "x": 471 + xOffset,   "y": 93 + yOffset},
+            { "x": 558 + xOffset,   "y": 93 + yOffset},
+            { "x": 558 + xOffset,  "y": 173 + yOffset},
+            { "x": 471 + xOffset,  "y": 173 + yOffset},
+            { "x": 471 + xOffset,   "y": 93 + yOffset}]
+          },
+  {name:"cabinet", // 64 82
+  id: 26,
+  points: [{ "x": 558 + xOffset,   "y": 93 + yOffset},
+            { "x": 575 + xOffset,   "y": 93 + yOffset},
+            { "x": 575 + xOffset,  "y": 139 + yOffset},
+            { "x": 558 + xOffset,  "y": 139 + yOffset},
+            { "x": 558 + xOffset,   "y": 93 + yOffset}]
+          }
+          ,
+  {name:"electrical", // 64 82
+  id: 27,
+  points: [{ "x": 575 + xOffset,   "y": 119 + yOffset},
+            { "x": 627 + xOffset,   "y": 119 + yOffset},
+            { "x": 627 + xOffset,  "y": 139 + yOffset},
+            { "x": 575 + xOffset,  "y": 139 + yOffset},
+            { "x": 575 + xOffset,   "y": 119 + yOffset}]
+          },
+  {name:"stairs2", // 64 82
+  id: 28,
+  points: [{ "x": 558 + xOffset,   "y": 138 + yOffset},
+            { "x": 641 + xOffset,   "y": 138 + yOffset},
+            { "x": 641 + xOffset,  "y": 173 + yOffset},
+            { "x": 558 + xOffset,  "y": 173 + yOffset},
+            { "x": 558 + xOffset,   "y": 138 + yOffset}]
+          },
+  {name:"lockers", // 64 82
+  id: 29,
+  points: [{ "x": 641 + xOffset,   "y": 149 + yOffset},
+            { "x": 706 + xOffset,   "y": 149 + yOffset},
+            { "x": 706 + xOffset,  "y": 173 + yOffset},
+            { "x": 641 + xOffset,  "y": 173 + yOffset},
+            { "x": 641 + xOffset,   "y": 149 + yOffset}]
+          },
+  {name:"409(shop)", // 64 82
+  id: 30,
+  points: [{ "x": 0 + xOffset,   "y": 183 + yOffset},
+            { "x": 58 + xOffset,   "y": 183 + yOffset},
+            { "x": 58 + xOffset,  "y": 300 + yOffset},
+            { "x": 0 + xOffset,  "y": 300 + yOffset},
+            { "x": 0 + xOffset,   "y": 183 + yOffset}]
+          }
+          ,
+  {name:"408", // 64 82
+  id: 31,
+  points: [{ "x": 58 + xOffset,   "y": 183 + yOffset},
+            { "x": 277 + xOffset,   "y": 183 + yOffset},
+            { "x": 277 + xOffset,  "y": 285 + yOffset},
+            { "x": 58 + xOffset,  "y": 285 + yOffset},
+            { "x": 58 + xOffset,   "y": 183 + yOffset}]
+          },
+  {name:"backElevator", // 64 82
+  id: 32,
+  points: [{ "x": 0 + xOffset,   "y": 300 + yOffset},
+            { "x": 58 + xOffset,   "y": 300 + yOffset},
+            { "x": 58 + xOffset,  "y": 326 + yOffset},
+            { "x": 0 + xOffset,  "y": 326 + yOffset},
+            { "x": 0 + xOffset,   "y": 300 + yOffset}]
+          },
+  {name:"stairs3", // 64 82
+  id: 33,
+  points: [{ "x": 58 + xOffset,   "y": 285 + yOffset},
+            { "x": 122 + xOffset,   "y": 285 + yOffset},
+            { "x": 122 + xOffset,  "y": 326 + yOffset},
+            { "x": 58 + xOffset,  "y": 326 + yOffset},
+            { "x": 58 + xOffset,   "y": 285 + yOffset}]
+          },
+  {name:"Mbathroom", // 64 82
+  id: 34,
+  points: [{ "x": 122 + xOffset,   "y": 285 + yOffset},
+            { "x": 150 + xOffset,   "y": 285 + yOffset},
+            { "x": 150 + xOffset,  "y": 331 + yOffset},
+            { "x": 122 + xOffset,  "y": 331 + yOffset},
+            { "x": 122 + xOffset,   "y": 285 + yOffset}]
+          },
+  {name:"Wbathroom", // 64 82
+  id: 35,
+  points: [{ "x": 150 + xOffset,   "y": 285 + yOffset},
+            { "x": 179 + xOffset,   "y": 285 + yOffset},
+            { "x": 179 + xOffset,  "y": 331 + yOffset},
+            { "x": 150 + xOffset,  "y": 331 + yOffset},
+            { "x": 150 + xOffset,   "y": 285 + yOffset}]
+          },
+  {name:"406", // 64 82
+  id: 36,
+  points: [{ "x": 277 + xOffset,   "y": 207 + yOffset},
+            { "x": 374 + xOffset,   "y": 207 + yOffset},
+            { "x": 374 + xOffset,  "y": 285 + yOffset},
+            { "x": 277 + xOffset,  "y": 285 + yOffset},
+            { "x": 277 + xOffset,   "y": 207 + yOffset}]
+          },
+  {name:"404", // 64 82
+  id: 37,
+  points: [{ "x": 374 + xOffset,   "y": 207 + yOffset},
+            { "x": 558 + xOffset,   "y": 207 + yOffset},
+            { "x": 558 + xOffset,  "y": 334 + yOffset},
+            { "x": 374 + xOffset,  "y": 334 + yOffset},
+            { "x": 374 + xOffset,   "y": 207 + yOffset}]
+          },
+  {name:"402", // 64 82
+  id: 38,
+  points: [{ "x": 558 + xOffset,   "y": 207 + yOffset},
+            { "x": 625 + xOffset,   "y": 207 + yOffset},
+            { "x": 625 + xOffset,  "y": 299 + yOffset},
+            { "x": 558 + xOffset,  "y": 299 + yOffset},
+            { "x": 558 + xOffset,   "y": 207 + yOffset}]
+          },
+  {name:"stairs4", // 64 82
+  id: 39,
+  points: [{ "x": 558 + xOffset,   "y": 299 + yOffset},
+            { "x": 640 + xOffset,   "y": 299 + yOffset},
+            { "x": 640 + xOffset,  "y": 334 + yOffset},
+            { "x": 558 + xOffset,  "y": 334 + yOffset},
+            { "x": 558 + xOffset,   "y": 299 + yOffset}]
+          },
+  {name:"elevators", // 64 82
+  id: 40,
+  points: [{ "x": 640 + xOffset,   "y": 309 + yOffset},
+            { "x": 709 + xOffset,   "y": 309 + yOffset},
+            { "x": 709 + xOffset,  "y": 334 + yOffset},
+            { "x": 640 + xOffset,  "y": 334 + yOffset},
+            { "x": 640 + xOffset,   "y": 309 + yOffset}]
+          },
+  {name:"401", // 64 82
+  id: 41,
+  points: [{ "x": 709 + xOffset,   "y": 285 + yOffset},
+            { "x": 744 + xOffset,   "y": 285 + yOffset},
+            { "x": 744 + xOffset,  "y": 334 + yOffset},
+            { "x": 709 + xOffset,  "y": 334 + yOffset},
+            { "x": 709 + xOffset,   "y": 285 + yOffset}]
+          }
+        ];
+
+
+var lineFunction = d3.svg.line()
+                        .x(function(d) { return d.x; })
+                        .y(function(d) { return d.y; })
+                        .interpolate("linear");
+function test() {
+  for(var i =0;i<roomData.length;i++)
+  {
+    var path =
+      d3.select("#mapSvg")
+      .append("path")
+      .attr("class","rooms")
+      .attr("id",roomData[i].name)
+      .attr("d", lineFunction(roomData[i].points))
+      .attr("stroke-width", 2)
+      .attr("fill", d3.hsl(25*roomData[i].id,0.5,0.5));
+    }
+}
+
+$(document).ready(function() {
+test()
+});
